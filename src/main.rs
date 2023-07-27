66ea208ec757178298d5d066912fab92d804c349
Yigit Aras <eytunali94@gmail.com>
2023-07-27 18:06:13 +0200

    Dielectrics working now... I think...

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,37 @@
 use kdam::tqdm;
-use rand::Rng;
 use rand::rngs::ThreadRng;
-use std::{fs::OpenOptions, io::BufWriter, io::Write};
+use rand::Rng;
 use std::rc::Rc;
+use std::{fs::OpenOptions, io::BufWriter, io::Write};
 
-
-
+mod camera;
 mod color;
 mod hittable;
 mod hittable_list;
+mod material;
 mod ray;
-mod vector3;
 mod sphere;
 mod utils;
-mod camera;
-mod material;
+mod vector3;
 
+use crate::camera::*;
 use crate::color::*;
 use crate::hittable::*;
 use crate::hittable_list::*;
+use crate::material::*;
 use crate::ray::*;
-use crate::vector3::*;
 use crate::sphere::*;
 use crate::utils::*;
-use crate::camera::*;
-use crate::material::*;
+use crate::vector3::*;
 
-fn ray_color(r: Ray, world: &dyn Hittable , rng: &mut ThreadRng, depth: u64) -> Vec3 {
-
+fn ray_color(r: Ray, world: &dyn Hittable, rng: &mut ThreadRng, depth: u64) -> Vec3 {
     if depth <= 0 {
         return Vec3::new(0.0, 0.0, 0.0);
     }
 
-    if let Some(hit) = world.hit(r, 0.00001, f64::MAX){
+    if let Some(hit) = world.hit(r, 0.00001, f64::MAX) {
         if let Some((scattered, attenuation)) = hit.mat_ptr.scatter(r, &hit) {
-            return ray_color(scattered, world, rng, depth-1) * attenuation;
+            return ray_color(scattered, world, rng, depth - 1) * attenuation;
         }
         return Vec3::new(0.0, 0.0, 0.0);
     }
@@ -44,11 +41,9 @@ fn ray_color(r: Ray, world: &dyn Hittable , rng: &mut ThreadRng, depth: u64) -> 
     Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
 }
 
-
 fn main() {
     println!("Program started...\n");
     let mut rng = rand::thread_rng();
-    
 
     // Image related
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
@@ -59,17 +54,41 @@ fn main() {
     // World
     let mut world: HittableList = Default::default();
 
-    let material_ground = Rc::new(Lambertian{albedo: Vec3::new(0.8,0.8,0.0)});
-    let material_center = Rc::new(Lambertian{albedo: Vec3::new(0.1,0.2,0.5)});
-    let material_left = Rc::new(Dielectric{ir: 1.5});
-    let material_left2 = Rc::new(Dielectric{ir: 1.5});
-    let material_right = Rc::new(Metal::new(Vec3::new(0.8,0.6,0.2), 1.0));
+    let material_ground = Rc::new(Lambertian {
+        albedo: Vec3::new(0.8, 0.8, 0.0),
+    });
+    let material_center = Rc::new(Lambertian {
+        albedo: Vec3::new(0.1, 0.2, 0.5),
+    });
+    let material_left = Rc::new(Dielectric { ir: 1.5 });
+    let material_left2 = Rc::new(Dielectric { ir: 1.5 });
+    let material_right = Rc::new(Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.0));
 
-    world.add(Box::new(Sphere::new(Vec3::new(0.0,-100.5,-1.0), 100.0, material_ground)));
-    world.add(Box::new(Sphere::new(Vec3::new(0.0,0.0,-1.0), 0.5, material_center)));
-    world.add(Box::new(Sphere::new(Vec3::new(-1.0, 0.0,-1.0), 0.5, material_left)));
-    world.add(Box::new(Sphere::new(Vec3::new(-1.0, 0.0,-1.0), -0.4, material_left2)));
-    world.add(Box::new(Sphere::new(Vec3::new(1.0,0.0,-1.0), 0.5, material_right)));
+    world.add(Box::new(Sphere::new(
+        Vec3::new(0.0, -100.5, -1.0),
+        100.0,
+        material_ground,
+    )));
+    world.add(Box::new(Sphere::new(
+        Vec3::new(0.0, 0.0, -1.0),
+        0.5,
+        material_center,
+    )));
+    world.add(Box::new(Sphere::new(
+        Vec3::new(-1.0, 0.0, -1.0),
+        0.5,
+        material_left,
+    )));
+    world.add(Box::new(Sphere::new(
+        Vec3::new(-1.0, 0.0, -1.0),
+        -0.4,
+        material_left2,
+    )));
+    world.add(Box::new(Sphere::new(
+        Vec3::new(1.0, 0.0, -1.0),
+        0.5,
+        material_right,
+    )));
 
     // Camera
     let cam = Camera::new();
@@ -94,16 +113,15 @@ fn main() {
     // Render
     for j in tqdm!((0..IMAGE_HEIGHT).rev(), animation = "fillup") {
         for i in 0..IMAGE_WIDTH {
-            let mut pixel_color = Vec3::new(0.0,0.0,0.0);
+            let mut pixel_color = Vec3::new(0.0, 0.0, 0.0);
 
-            for _ in 0..SAMPLES_PER_PIXEL{
-                let u = (i as f64 + rng.gen_range(0.0..1.0)) / ((IMAGE_WIDTH-1) as f64);
-                let v = (j as f64 + rng.gen_range(0.0..1.0)) / ((IMAGE_HEIGHT-1) as f64);
+            for _ in 0..SAMPLES_PER_PIXEL {
+                let u = (i as f64 + rng.gen_range(0.0..1.0)) / ((IMAGE_WIDTH - 1) as f64);
+                let v = (j as f64 + rng.gen_range(0.0..1.0)) / ((IMAGE_HEIGHT - 1) as f64);
                 let r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, &world, &mut rng, DEPTH-1);
+                pixel_color += ray_color(r, &world, &mut rng, DEPTH - 1);
             }
 
-            
             write_color(&mut file, pixel_color, SAMPLES_PER_PIXEL);
         }
     }
--- src/material.rs
+++ src/material.rs
@@ -1,29 +1,29 @@
-use crate::utils::*;
 use crate::hittable::*;
-use crate::vector3::*;
 use crate::ray::*;
+use crate::utils::*;
+use crate::vector3::*;
 
-use rand::Rng;
 use dyn_clone::*;
+use rand::Rng;
 
 fn random_in_unit_sphere() -> Vec3 {
     let mut rng = rand::thread_rng();
     let unit = Vec3::new(1.0, 1.0, 1.0);
     loop {
-        let p =  Vec3::new(rng.gen::<f64>(), rng.gen::<f64>(), rng.gen::<f64>()) * 2.0  - unit;
+        let p = Vec3::new(rng.gen::<f64>(), rng.gen::<f64>(), rng.gen::<f64>()) * 2.0 - unit;
         if p.lenght_squared() < 1.0 {
-            return p
+            return p;
         }
     }
 }
 
-fn reflect(v: Vec3, n: Vec3) -> Vec3{
-    v -  n * v.dot(n) * 2.0
+fn reflect(v: Vec3, n: Vec3) -> Vec3 {
+    v - n * v.dot(n) * 2.0
 }
 
-fn refract(uv: Vec3, n: Vec3, etai_over_etat: f64) -> Vec3{
+fn refract(uv: Vec3, n: Vec3, etai_over_etat: f64) -> Vec3 {
     let cos_theta = f64::min(-uv.dot(n), 1.0);
-    let r_out_perp = (uv+n*cos_theta) * etai_over_etat;
+    let r_out_perp = (uv + n * cos_theta) * etai_over_etat;
     let r_out_parallel = n * (-f64::sqrt(f64::abs(1.0 - r_out_perp.lenght_squared())));
     r_out_perp + r_out_parallel
 }
@@ -31,10 +31,10 @@ fn refract(uv: Vec3, n: Vec3, etai_over_etat: f64) -> Vec3{
 fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
     // Schlick approximation
     let r0 = ((1.0 - ref_idx) / (1.0 + ref_idx)).powi(2);
-    r0 + (1.0 -r0) * (1.0 - cosine).powi(5)
+    r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
 }
 
-pub trait Material: DynClone  {
+pub trait Material: DynClone {
     fn scatter(&self, r_in: Ray, hit: &HitRecord) -> Option<(Ray, Vec3)>;
 }
 
@@ -42,30 +42,25 @@ dyn_clone::clone_trait_object!(Material);
 
 #[derive(Clone, Copy)]
 pub struct Lambertian {
-    pub albedo: Vec3
+    pub albedo: Vec3,
 }
 
 #[derive(Clone, Copy)]
 pub struct Metal {
     pub albedo: Vec3,
-    pub fuzz: f64
+    pub fuzz: f64,
 }
 
 #[derive(Clone, Copy)]
 pub struct Dielectric {
-    pub ir: f64
+    pub ir: f64,
 }
 
 impl Metal {
     pub fn new(a: Vec3, f: f64) -> Self {
         Self {
             albedo: a,
-            fuzz: if f < 1.0 {
-                f
-            } else {
-                1.0
-            }
-
+            fuzz: if f < 1.0 { f } else { 1.0 },
         }
     }
 }
@@ -73,11 +68,11 @@ impl Metal {
 impl Material for Lambertian {
     fn scatter(&self, r_in: Ray, hit: &HitRecord) -> Option<(Ray, Vec3)> {
         let mut scatter_dir = hit.normal + random_in_unit_sphere();
-        
+
         if scatter_dir.near_zero() {
             scatter_dir = hit.normal;
         }
-        
+
         let scattered = Ray::new(hit.p, scatter_dir);
         let attenuation = self.albedo;
         Some((scattered, attenuation))
@@ -86,10 +81,11 @@ impl Material for Lambertian {
 
 impl Material for Metal {
     fn scatter(&self, r_in: Ray, hit: &HitRecord) -> Option<(Ray, Vec3)> {
-        let reflected = reflect(Vec3::unit_vector(r_in.dir), hit.normal) + random_in_unit_sphere()* self.fuzz;
+        let reflected =
+            reflect(Vec3::unit_vector(r_in.dir), hit.normal) + random_in_unit_sphere() * self.fuzz;
         let scattered = Ray::new(hit.p, reflected);
         let attenuation = self.albedo;
-        
+
         if reflected.dot(hit.normal) > 0.0 {
             Some((scattered, attenuation))
         } else {
@@ -99,25 +95,29 @@ impl Material for Metal {
 }
 
 impl Material for Dielectric {
-    fn scatter(&self, r_in: Ray, hit: &HitRecord) -> Option<(Ray, Vec3)> {
-        let mut rng = rand::thread_rng();
-        let attenuation = Vector3::new(1.0, 1.0, 1.0);
-        let (outward_normal, ni_over_nt, cosine) = if ray.direction().dot(&hit.normal) > 0.0 {
-            let cosine = self.ref_idx * ray.direction().dot(&hit.normal) / ray.direction().magnitude();
-            (-hit.normal, self.ref_idx, cosine)
+    fn scatter(&self, ray: Ray, hit: &HitRecord) -> Option<(Ray, Vec3)> {
+        let attenuation = Vec3::new(1.0, 1.0, 1.0);
+        let (outward_normal, refraction_ratio, cos_theta) = if ray.dir.dot(hit.normal) > 0.0 {
+            let cos_theta = self.ir * ray.dir.dot(hit.normal) / ray.dir.length();
+            (-hit.normal, self.ir, cos_theta)
         } else {
-            let cosine = -ray.direction().dot(&hit.normal) / ray.direction().magnitude();
-            (hit.normal, 1.0 / self.ref_idx, cosine)
+            let cos_theta = -ray.dir.dot(hit.normal) / ray.dir.length();
+            (hit.normal, 1.0 / self.ir, cos_theta)
         };
-        if let Some(refracted) = refract(&ray.direction(), &outward_normal, ni_over_nt) {
-            let reflect_prob = schlick(cosine, self.ref_idx);
-            if rand::thread_rng().gen::<f32>() >= reflect_prob {
-                let scattered = Ray::new(hit.p, refracted);
-                return Some((scattered, attenuation))
-            }
-        }
-        let reflected = reflect(&ray.direction(), &hit.normal);
-        let scattered = Ray::new(hit.p, reflected);
+        let unit_dir = Vec3::unit_vector(ray.dir);
+        let sin_theta = (1.0 - cos_theta.powi(2)).sqrt();
+
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+
+        let direction = if cannot_refract
+            || reflectance(cos_theta, refraction_ratio) > rand::thread_rng().gen::<f64>()
+        {
+            reflect(unit_dir, outward_normal)
+        } else {
+            refract(unit_dir, outward_normal, refraction_ratio)
+        };
+
+        let scattered = Ray::new(hit.p, direction);
         Some((scattered, attenuation))
     }
 }
