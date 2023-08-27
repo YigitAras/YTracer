ac6c614414842d8384c460cecf8f49a61953a8b2
YigitAras <eytunali94@gmail.com>
2023-08-27 12:49:37 +0200

    More work on Perlin Noise

--- src/perlin.rs
+++ src/perlin.rs
@@ -1,6 +1,11 @@
 /*
  *   Struct and necessary methods to create Perlin noise
  */
+use crate::vector3::*;
+
+use rand::Rng;
+use rand::rngs::ThreadRng;
+
 pub struct Perlin {
     rand_floats: Vec<f64>,
     perm_x: Vec<i64>,
@@ -19,21 +24,32 @@ impl Perlin {
         unimplemented!()
     }
 
-    pub fn noise(&self) -> f64{
-        0.0
+    pub fn noise(&self, p: Vec3) -> f64{
+        let i = ((4.0 * p.x) as i64) & 255;
+        let j = ((4.0 * p.y) as i64) & 255;
+        let k = ((4.0 * p.z) as i64) & 255;
+
+        self.rand_floats[self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize]]
     }
 
-    fn permute(p: &Vec<i64>, n: usize) {
-        unimplemented!()
+    // TODO: Rustify the loop?
+    fn permute(p: &mut [i64], n: usize) {
+        let mut rng = rand::thread_rng();
+        for i in  (1..n).rev() {
+            let target: usize = rng.gen_range(0..i);
+            // Todo: Not sure of this operation
+            p.swap(i, target);
+        }
     }
+    // TODO: Rustify the loop?
     fn perlin_generate_perm() -> Vec<i64> {
         let mut p: Vec<i64> = vec![0; Perlin::point_count()];
         for i in 0..Perlin::point_count() {
             p[i] = i as i64;
         }
 
-        Perlin::permute(&p, Perlin::point_count());
-        
+        Perlin::permute(&mut p, Perlin::point_count());
+
         p
     }
 }
